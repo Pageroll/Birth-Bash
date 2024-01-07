aea8ea5e7a8fa7c2fa9e5271d2da18d0aa63f75e
Gaurav Patel <gp43883@gmail.com>
2024-01-07 18:50:41 +0530

    MongoDb URI exposed issue solved

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require("dotenv").config()
 
 const mongo = ()=>{
-     mongoose.connect(`mongodb+srv://robertjr:pg123456@cluster0.ljveogm.mongodb.net/db`,).then(()=>{
+     mongoose.connect(`${process.env.CONN_STR}`,).then(()=>{
     console.log("DB connected")
     // console.log(process.env.DB_CONNECT)
   }).catch((err)=>console.log(err));
